d1795fdd6497fe2bcfadb8121c9bdffd33894f9f
tayoon <ta.tsu.1067@gmail.com>
2021-04-01 11:30:59 +0900

    AC

--- ABC086/ABC086C.cpp
+++ ABC086/ABC086C.cpp
@@ -8,12 +8,13 @@ int main() {
 
     t[0] = x[0] = y[0] = 0;
 
-    bool test = false;
+    bool test = true;
     for (int i = 1; i <= n; i++) {
         cin >> t[i] >> x[i] >> y[i];
         int dt = t[i] - t[i-1];
         int dxy = abs(x[i]-x[i-1]) + abs(y[i]-y[i-1]);
-        if (dt >= dxy && dt%2 == dxy%2) test = true;
+        if (dt < dxy) test = false;
+        if (dt%2 != dxy%2) test = false;
     }
 
     if(test) cout << "Yes" << endl;
