6ac5a445ac02123ba9760ee1463f12f45873101e
tayoon <ta.tsu.1067@gmail.com>
2021-04-01 11:54:02 +0900

    AC

---
+++ ABC064/ABC064A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, g, b;
+    cin >> r >> g >> b;
+
+    int total = r*100 + g*10 + b;
+
+    if (total % 4 == 0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
