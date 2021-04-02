98289893bd91804d45d6a37c2b79f3197aa9b997
tayoon <ta.tsu.1067@gmail.com>
2021-04-02 09:56:29 +0900

    AC x7 WA x5

---
+++ ABC113/ABC113B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int t, a;
+    cin >> t >> a;
+
+    int h[n], diff, res;
+    int min = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        cin >> h[i];
+        diff = abs(a - (t-h[i]*0.006));
+        if (min > diff) {
+            res = i + 1;
+            min = diff;
+        }
+    }
+
+    cout << res << endl;
+}
