20b9ce6db7febcb2f64862e2b5f6ff9fc3a159da
tayoon <ta.tsu.1067@gmail.com>
2021-04-03 21:55:08 +0900

    update

--- ABC007/ABC007C.cpp
+++ ABC007/ABC007C.cpp
@@ -1,39 +1,56 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+// using Graph = vector<vector<int>>;
 
 int r, c;
 int sx, sy, gx, gy;
 int a[52][52];
 char ch;
 
-void bfs(int x1, int y1, int g){
-	if(a[x1][y1] == -1) return;
-	if(a[x1][y1] != 0 && a[x1][y1] <= g) return;
-	a[x1][y1] = g;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-	bfs(x1+1, y1, g+1);
-	bfs(x1-1, y1, g+1);
-	bfs(x1, y1+1, g+1);
-	bfs(x1, y1-1, g+1);
-}
+// 未到達:0
+// 壁:-1
+// 到達済:1
 
 int main(){
 	cin >> r >> c >> sx >> sy >> gx >> gy;
-	for(int i = 0; i <= 51; i++){
-		for(int j = 0; j <= 51; j++){
-			a[i][j]=-1;
+
+	// Graph G((r-2)*(c-2));
+	
+	for(int i = 0; i < r; i++){
+		for(int j = 0; j < c; j++){
+			cin >> ch;
+			if(ch == '.') a[i][j] = 0;
+			else a[i][j] = -1;
         }
     }
 
-	for(int i = 1; i <= r; i++){
-		for(int j = 1; j <= c; j++){
-			cin >> ch;
-			if(ch == '.')
-				a[i][j] = 0;
-		}
-    }
+	vector<vector<int> > dist(r, vector<int>(c, -1));
+	dist[sx][sy] = 1;
 
-	bfs(sx, sy, 0);
-	cout << a[gx][gy] << endl;
-	return 0;
+	queue<pair<int, int> > que;
+	que.push(make_pair(sx, sy));
+
+	while (!que.empty()) {
+		pair<int, int> current_pos = que.front();
+		int x = current_pos.first;
+		int y = current_pos.second;
+		que.pop();
+
+		for (int direction = 0; direction < 4; direction++) {
+			int next_x = x + dx[direction];
+			int next_y = y + dy[direction];
+			if (next_x < 0 || next_x > c || next_y < 0 || next_y > r) continue;		// マスはみ出し
+			if (a[next_x][next_y] == -1) continue;		// 壁
+
+			if (dist[next_x][next_y] == -1) {
+                dist[next_x][next_y] = dist[x][y] + 1;
+                que.push(make_pair(next_x, next_y));
+            }
+		}
+	}
+
 }
---
+++ ABC047/ABC047B.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int w, h, n;
+    cin >> w >> h >> n;
+
+    for (int i = 0; i < n; i++) {
+        
+    }
+}
