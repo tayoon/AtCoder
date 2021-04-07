7e27bf4425409ba5f54e126c4f8511a3892ef877
tayoon <ta.tsu.1067@gmail.com>
2021-04-07 21:57:54 +0900

    AC

--- ABC059/ABC059C.cpp
+++ ABC059/ABC059C.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -9,7 +9,7 @@ int main() {
         cin >> a[i];
     }
 
-    int sum = 0, res1 = 0, res2 = 0;
+    long long sum = 0, res1 = 0, res2 = 0;
     for (int i = 0; i < n; i++) {
         sum += a[i];
         if (i%2 == 0 && sum <= 0) {
