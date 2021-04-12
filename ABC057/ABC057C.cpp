037c7a9d587293ffd3ab836ea38a1a2f3bd27c74
tayoon <ta.tsu.1067@gmail.com>
2021-04-12 22:21:30 +0900

    AC x11 WA x4

--- ABC057/ABC057C.cpp
+++ ABC057/ABC057C.cpp
@@ -10,56 +10,16 @@ int Digit (int n) {
     return digit;
 }
 
-int Compare (int a, int b) {
-    int res_a = Digit(a);
-    int res_b = Digit(b);
-    cout << "res_a:" << res_a << "*res_b:" << res_b << endl;
-
-    return max(res_a, res_b);
-}
-
 int main() {
-    long n;
+    long long n;
     cin >> n;
     
-    int num = 0;
-    int data[100000] = {};
-    for (int i = 1; i <= sqrt(n); i++) {
+    int mini = INT_MAX;
+    for (long long i = 1; i * i <= n; i++) {
         if (n % i == 0) {
-            data[num] = i;
-            num++;
+            mini = min(mini, max(Digit(i), Digit(n/i)));
         }
     }
 
-    int min = INT_MAX;
-    for (int i = 0; i < num; i++) {
-        long anoth = n / data[i];
-        cout << "i:" << i << " " << data[i] << ":" << anoth << endl;
-        
-        int res = Compare(data[i], anoth);
-        if (res < min) {
-            min = res;
-            cout << data[i] << ":" << anoth << "---min ver---" << " compare:" << min << endl;
-        }
-    }
-
-    cout << min << endl;
+    cout << mini << endl;
 }
-
-
-
-/*
- * output
- * 9876543210
-1:9876543210
-res_a:1*res_b:10
-1:9876543210---min ver--- compare:10
-2:4938271605
-res_a:1*res_b:9
-2:4938271605---min ver--- compare:9
-3:3292181070
-res_a:1*res_b:0
-3:3292181070---min ver--- compare:1
-5:1975308642
-res_a:1*res_b:10
-*/
