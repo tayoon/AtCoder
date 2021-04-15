5b4a04da4b03009b2145cfa0c156ced5a2c4c543
tayoon <ta.tsu.1067@gmail.com>
2021-04-15 14:57:53 +0900

    AC

--- ABC075/ABC075B.cpp
+++ ABC075/ABC075B.cpp
@@ -8,21 +8,21 @@ int main() {
     int dx[8] = {1, 0, -1, -1, -1, 0, 1, 1};
     int dy[8] = {1, 1, 1, 0, -1, -1, -1, 0};
 
-    char a[h][w];
-    int cnt[h][w] = {};
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
+    char a[h+2][w+2] = {};
+    int cnt[h+2][w+2] = {};
+    for (int i = 1; i < h+1; i++) {
+        for (int j = 1; j < w+1; j++) {
             cin >> a[i][j];
             if (a[i][j] == '#') {
-                for (int i = 0; i < 8; i++) {
-                    cnt[i-dx[i]][j-dy[i]]++;
+                for (int k = 0; k < 8; k++) {
+                    cnt[i+dx[k]][j+dy[k]]++;
                 }
             }
         }
     }
 
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
+    for (int i = 1; i < h+1; i++) {
+        for (int j = 1; j < w+1; j++) {
             if (a[i][j] == '#') {
                 cout << "#";
             } else {
