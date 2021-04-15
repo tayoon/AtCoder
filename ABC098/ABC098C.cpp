d3aa7b71429ddc0027cbf35e57a0df4c87b57813
tayoon <ta.tsu.1067@gmail.com>
2021-04-15 16:37:04 +0900

    AC x6 TLE x20

---
+++ ABC098/ABC098C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int cnt[n] = {};
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            for (int j = 1; j < n; j++) {
+                if (s[j] == 'E') cnt[i]++;
+            }
+        } else if (i == 4) {
+            for (int j = 0; j < n-1; j++) {
+                if (s[j] == 'W') cnt[i]++;
+            }
+        } else {
+            for (int j = 0; j < i; j++) {
+                if (s[j] == 'W') cnt[i]++;
+            }
+            for (int k = i+1; k < n; k++) {
+                if (s[k] == 'E') cnt[i]++;
+            }
+        }
+    }
+
+    cout << *min_element(cnt, cnt + n) << endl;
+}
