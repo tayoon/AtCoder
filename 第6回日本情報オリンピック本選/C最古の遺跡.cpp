1982156d62f5bc4d89723a735c3a0ef8e6d2c74b
tayoon <ta.tsu.1067@gmail.com>
2021-04-24 21:59:44 +0900

    No

---
+++ 第6回日本情報オリンピック本選/C最古の遺跡.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> pillar(n, vector<int>(2)); 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> pillar[i][j];
+        }
+    }
+
+    
+}
