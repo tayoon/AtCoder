62f2c64c5e0d0556bcff087aa9de813a14341bfa
tayoon <ta.tsu.1067@gmail.com>
2021-04-26 23:19:09 +0900

    segmentation fault

--- 第6回日本情報オリンピック本選/C最古の遺跡.cpp
+++ 第6回日本情報オリンピック本選/C最古の遺跡.cpp
@@ -5,12 +5,29 @@ int main() {
     int n;
     cin >> n;
 
-    vector<vector<int>> pillar(n, vector<int>(2)); 
+    vector<int> x(n), y(n);
+    vector<vector<bool>> map(5001, vector<bool>(5001)); 
+
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 2; j++) {
-            cin >> pillar[i][j];
+        cin >> x[i] >> y[i];
+        map[y[i]][x[i]] = true;
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+
+            int dx = x[j] - x[i];
+            int dy = y[j] - y[i];
+
+            if (y[i]-dx < 0 || x[i]+dy > 5000 || y[j]-dx < 0 || x[j]+dy > 5000) continue;
+
+            if (map[y[i]-dx][x[i]+dy] && map[y[j]-dx][x[j]+dy]) {
+                res = max(res, dx*dx + dy*dy);
+            }
         }
     }
 
-    
+    cout << res << endl;
 }
