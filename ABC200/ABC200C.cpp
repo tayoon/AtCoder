b2da9f6be9ba5fb18a91a13d6308ddcad2a69baa
tayoon <ta.tsu.1067@gmail.com>
2021-05-08 22:07:42 +0900

    Ac x21 WA x5

--- ABC200/ABC200C.cpp
+++ ABC200/ABC200C.cpp
@@ -6,15 +6,20 @@ int main() {
     cin >> n;
 
     vector<long long> a(n, 0);
-    int cnt = 0;
+    vector<int> cnt(n, 0);
+    int all = 0;
 
     for (int j = 0; j < n; j++) {
         cin >> a[j];
         if (j == 0) continue;
-        for (int i = 0; i < j; i++) {
-            if (abs(a[i]-a[j]) % 200 == 0) cnt++;
+        for (int i = j-1; i >= 0; i--) {
+            if (abs(a[i]-a[j]) % 200 == 0) {
+                cnt[j] = cnt[i] + 1;
+                break;
+            }
         }
+        all += cnt[j];
     }
 
-    cout << cnt << endl;
+    cout << all << endl;
 }
