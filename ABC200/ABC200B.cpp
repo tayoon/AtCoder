8a505164fe0f606079cf09d07448e9a5b8d8405d
tayoon <ta.tsu.1067@gmail.com>
2021-05-08 21:12:49 +0900

    AC

---
+++ ABC200/ABC200B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++) {
+        if (n % 200 == 0) n /= 200;
+        else {
+            n = n * 1000 + 200;
+        }
+    }
+
+    cout << n << endl;
+}
