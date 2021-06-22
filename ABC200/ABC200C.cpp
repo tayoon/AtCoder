450f908a75d86b6189ec4bbee1d6f0626ee5b095
tayoon <ta.tsu.1067@gmail.com>
2021-06-22 21:17:22 +0900

    update prettier

--- ABC200/ABC200C.cpp
+++ ABC200/ABC200C.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+  int n;
+  cin >> n;
 
-    vector<long long> a(n, 0);
-    vector<long long> cnt(n, 0);
-    long long ans = 0;
+  vector<long long> a(n, 0);
+  vector<long long> cnt(n, 0);
+  long long ans = 0;
 
-    for (int j = 0; j < n; j++) {
-        cin >> a[j];
-        if (j == 0) continue;
-        
-        for (int i = j-1; i >= 0; i--) {
-            if (abs(a[i]-a[j]) % 200 == 0) {
-                cnt[j] = cnt[i] + 1;
-                break;
-            }
-        }
-        ans += cnt[j];
+  for (int j = 0; j < n; j++) {
+    cin >> a[j];
+    if (j == 0) continue;
+
+    for (int i = j - 1; i >= 0; i--) {
+      if (abs(a[i] - a[j]) % 200 == 0) {
+        cnt[j] = cnt[i] + 1;
+        break;
+      }
     }
+    ans += cnt[j];
+  }
 
-    cout << ans << endl;
+  cout << ans << endl;
 }
